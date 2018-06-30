d87faf2e714fd32da73ce0e1aac9994061a13981
arthurtruong <arthurtruong.work@gmail.com>
2018-06-30 19:21:32 +1000

    added images, added logo, added css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,6 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
